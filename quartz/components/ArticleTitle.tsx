68926f1db26be303abefffcbb54e3c771c466cc4
rbmr <robertmertens10@gmail.com>
2026-02-22 01:54:46 +0100

    feat: disable graph, update gitignore for easy local testing, update article title component for prettier naming of index files and folders, update custom.scss to better match obsidian.

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,8 +1,26 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
-const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const title = fileData.frontmatter?.title
+const ArticleTitle: QuartzComponent = ({ fileData, displayClass, cfg }: QuartzComponentProps) => {
+  let title = fileData.frontmatter?.title
+
+  // Check if the title is defaulting to "index" or is an auto-generated folder title
+  const isDefaultTitle = title === "index" || title?.startsWith("Folder: ")
+
+  if (isDefaultTitle) {
+    if (fileData.slug === "index") {
+      // Home Page -> Use the site name from quartz.config.ts
+      title = cfg.pageTitle
+    } else if (fileData.slug?.endsWith("/index")) {
+      // Folder page
+      const pathSegments = fileData.slug.split("/")
+      const folderSlug = pathSegments[pathSegments.length - 2]
+      title = folderSlug
+        .replace(/-/g, " ")
+        .replace(/\b\w/g, (char) => char.toUpperCase())
+    }
+  }
+
   if (title) {
     return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
   } else {
